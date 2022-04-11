33975dae9aaef20bd275889eb2d4d3e0ae6f20bd
Matheus dos Reis <mathreis1999@gmail.com>
2022-04-11 20:25:56 -0300

    Utilizando reaproveitamento de codigo (setup e cleanup) e implementando o dispose

--- Alura.Estacionamento.Testes/PatioTestes.cs
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -1,29 +1,44 @@
 ﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
 using Alura.Estacionamento.Modelos;
+using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Alura.Estacionamento.Testes
 {
-    public class PatioTestes
+    public class PatioTestes : IDisposable
     {
+        private Veiculo _veiculo;
+        private Patio _patio;
+        public ITestOutputHelper saidaConsoleTeste;
+
+        public PatioTestes(ITestOutputHelper saidaConsoleTeste)
+        {
+            this.saidaConsoleTeste = saidaConsoleTeste;
+            this.saidaConsoleTeste.WriteLine("Executando o construtor");
+
+            _veiculo = new Veiculo();
+            _patio = new Patio();
+        }
+
         [Fact]
         public void Testa_Metodo_Total_Faturado_Com_Um_Veiculo()
         {
-            var estacionamento = new Patio();
-            var veiculo = new Veiculo();
+            //var estacionamento = new Patio();
 
-            veiculo.Proprietario = "Fulado de Tal";
-            veiculo.Cor = "Preto";
-            veiculo.Modelo = "Carro";
-            veiculo.Placa = "abc-1234";
-            veiculo.Tipo = TipoVeiculo.Automovel;
+            //var veiculo = new Veiculo();
+            _veiculo.Proprietario = "Fulado de Tal";
+            _veiculo.Cor = "Preto";
+            _veiculo.Modelo = "Carro";
+            _veiculo.Placa = "abc-1234";
+            _veiculo.Tipo = TipoVeiculo.Automovel;
 
-            estacionamento.RegistrarEntradaVeiculo(veiculo);
-            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+            _patio.RegistrarEntradaVeiculo(_veiculo);
+            _patio.RegistrarSaidaVeiculo(_veiculo.Placa);
 
 
 
-            var faturamento = estacionamento.TotalFaturado();
+            var faturamento = _patio.TotalFaturado();
 
 
 
@@ -36,20 +51,20 @@ namespace Alura.Estacionamento.Testes
         [InlineData("Fulano de Tal Primo", "GEF-9876", "Vermelho", "Moto")]
         public void Testa_Metodo_Total_Faturado_Com_Varios_Veiculos(string proprietario, string placa, string cor, string modelo)
         {
-            var estacionamento = new Patio();
-            var veiculo = new Veiculo();
+            //var estacionamento = new Patio();
 
-            veiculo.Proprietario = proprietario;
-            veiculo.Placa = placa;
-            veiculo.Cor = cor;
-            veiculo.Modelo = modelo;
+            //var veiculo = new Veiculo();
+            _veiculo.Proprietario = proprietario;
+            _veiculo.Placa = placa;
+            _veiculo.Cor = cor;
+            _veiculo.Modelo = modelo;
 
-            estacionamento.RegistrarEntradaVeiculo(veiculo);
-            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+            _patio.RegistrarEntradaVeiculo(_veiculo);
+            _patio.RegistrarSaidaVeiculo(_veiculo.Placa);
 
 
 
-            var faturamento = estacionamento.TotalFaturado();
+            var faturamento = _patio.TotalFaturado();
 
 
 
@@ -60,19 +75,19 @@ namespace Alura.Estacionamento.Testes
         [InlineData("Fulano de Tal", "ABC-1234", "Preto", "Carro")]
         public void Testa_Metodo_Pesquisa_Veiculo(string proprietario, string placa, string cor, string modelo)
         {
-            var estacionamento = new Patio();
-            var veiculo = new Veiculo();
+            //var estacionamento = new Patio();
 
-            veiculo.Proprietario = proprietario;
-            veiculo.Placa = placa;
-            veiculo.Cor = cor;
-            veiculo.Modelo = modelo;
+            //var veiculo = new Veiculo();
+            _veiculo.Proprietario = proprietario;
+            _veiculo.Placa = placa;
+            _veiculo.Cor = cor;
+            _veiculo.Modelo = modelo;
 
-            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            _patio.RegistrarEntradaVeiculo(_veiculo);
 
 
 
-            var consultado = estacionamento.PesquisaVeiculo(placa);
+            var consultado = _patio.PesquisaVeiculo(placa);
 
 
 
@@ -82,14 +97,17 @@ namespace Alura.Estacionamento.Testes
         [Fact]
         public void Testa_Metodo_Alterar_Dados_Veiculo_Alterando_Placa()
         {
-            var estacionamento = new Patio();
-            var veiculo = new Veiculo
-            { 
-                Proprietario = "Fulano",
-                Placa = "ABC-1234",
-                Cor = "Preto",
-                Modelo = "Carro",
-            };
+            //var estacionamento = new Patio();
+
+            //var veiculo = new Veiculo();
+            _veiculo.Proprietario = "Fulano";
+            _veiculo.Placa = "ABC-1234";
+            _veiculo.Cor = "Preto";
+            _veiculo.Modelo = "Carro";
+            _veiculo.Tipo = TipoVeiculo.Automovel;
+
+            _patio.RegistrarEntradaVeiculo(_veiculo);
+
             var veiculoAlterado = new Veiculo
             {
                 Proprietario = "Fulano",
@@ -97,15 +115,19 @@ namespace Alura.Estacionamento.Testes
                 Cor = "Branco", //alteração
                 Modelo = "Carro",
             };
-            estacionamento.RegistrarEntradaVeiculo(veiculo);
 
 
 
-            var alterado = estacionamento.AlterarDadosVeiculo(veiculoAlterado);
+            var alterado = _patio.AlterarDadosVeiculo(veiculoAlterado);
 
 
 
             Assert.Equal(alterado.Cor, veiculoAlterado.Cor);
+        }
+
+        public void Dispose()
+        {
+            saidaConsoleTeste.WriteLine("Executando o dispose");
         }
     }
 }
--- Alura.Estacionamento.Testes/VeiculoTestes.cs
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -1,32 +1,52 @@
 using Alura.Estacionamento.Alura.Estacionamento.Modelos;
 using Alura.Estacionamento.Modelos;
+using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Alura.Estacionamento.Testes
 {
-    public class VeiculoTestes
+    public class VeiculoTestes : IDisposable
     {
+        private Veiculo _veiculo; //setup
+        public ITestOutputHelper saidaConsoleTeste;
+
+        public VeiculoTestes(ITestOutputHelper saidaConsoleTeste) //sempre criado o ctor antes da chamada do metodo de teste
+        {
+            this.saidaConsoleTeste = saidaConsoleTeste;
+            this.saidaConsoleTeste.WriteLine("Executando o construtor");
+
+            _veiculo = new Veiculo();
+        }
+
         [Fact/*(DisplayName = "Teste para aceleração de um veículo")*/]
         //[Trait("Funcionalidade", "Acelerar")]
         public void Testa_Metodo_Acelerar_Com_Parametro_Dez()
         {
             // AAA
             //Arrenge (CONFIGURAÇÃO)
-            var veiculo = new Veiculo();
+            //var veiculo = new Veiculo();
+
             //Act (AÇÃO)
-            veiculo.Acelerar(10); //10 * 10 + velocidade atual
+            _veiculo.Acelerar(10); //10 * 10 + velocidade atual
+
             //Assert (VALIDAÇÃO)
-            Assert.Equal(100, veiculo.VelocidadeAtual);
+            Assert.Equal(100, _veiculo.VelocidadeAtual);
         }
 
         [Fact]
         //[Trait("Funcionalidade", "Frear")]
         public void Testa_Metodo_Frear_Com_Parametro_Dez()
         {
-            var veiculo = new Veiculo();
-            veiculo.Frear(10); //10 * 15 - velocidade atual
+            //var veiculo = new Veiculo();
 
-            Assert.Equal(-150, veiculo.VelocidadeAtual);
+
+
+            _veiculo.Frear(10); //10 * 15 - velocidade atual
+
+
+
+            Assert.Equal(-150, _veiculo.VelocidadeAtual);
         }
 
         [Fact(Skip = "Teste em implementação")]
@@ -38,22 +58,26 @@ namespace Alura.Estacionamento.Testes
         [Fact]
         public void Testa_Dados_Do_Veiculo()
         {
-            var veiculo = new Veiculo
-            {
-                Proprietario = "Fulano",
-                Placa = "ABC-1234",
-                Cor = "Preto",
-                Modelo = "Carro",
-                Tipo = TipoVeiculo.Automovel
-            };
+            //var veiculo = new Veiculo();
+            _veiculo.Proprietario = "Fulano";
+            _veiculo.Placa = "ABC-1234";
+            _veiculo.Cor = "Preto";
+            _veiculo.Modelo = "Carro";
+            _veiculo.Tipo = TipoVeiculo.Automovel;
 
 
 
-            string dados = veiculo.ToString();
+
+            string dados = _veiculo.ToString();
 
 
 
             Assert.Contains("Tipo do Veículo: Automovel", dados);
+        }
+
+        public void Dispose()
+        {
+            saidaConsoleTeste.WriteLine("Executando o dispose");
         }
     }
 }
