dc066f1d19264f239a1b42952cf9f10ebf77aec4
Matheus dos Reis <mathreis1999@gmail.com>
2022-04-11 20:28:24 -0300

    atualizando modelo veiculo

--- Alura.Estacionamento/Alura.Estacionamento.Modelos/Veiculo.cs
+++ Alura.Estacionamento/Alura.Estacionamento.Modelos/Veiculo.cs
@@ -65,7 +65,18 @@ namespace Alura.Estacionamento.Modelos
         public string Modelo { get; set; }
         public string Proprietario
         {
-            get; set;
+            get
+            {
+                return _proprietario;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new System.FormatException(" Nome de proprietário deve ter no mínimo 3 caracteres.");
+                }
+                _proprietario = value;
+            }
         }
         public DateTime HoraEntrada { get; set; }
         public DateTime HoraSaida { get; set; }
