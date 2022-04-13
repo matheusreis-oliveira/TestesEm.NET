ba7a74a49d1c4dc902500dfc96c3f22cf33664b0
Matheus dos Reis <mathreis1999@gmail.com>
2022-04-13 16:54:35 -0300

    Adicionando primeiro teste de infrestutura

---
+++ TestesIntegracao/Alura.ByteBank.Infraestrutura.Testes/ByteBankContextoTestes.cs
@@ -0,0 +1,29 @@
+﻿using Alura.ByteBank.Dados.Contexto;
+using System;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ByteBankContextoTestes
+    {
+        [Fact]
+        public void TestaConexaoContextoComBDMySQL()
+        {
+            //Arrange
+            var contexto = new ByteBankContexto();
+            bool conectado;
+            //Act
+            try
+            {
+                conectado = contexto.Database.CanConnect();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Não foi possível conectar a base de dados.[{e.Message}]");
+
+            }
+            //Assert
+            Assert.True(conectado);
+        }
+    }
+}
